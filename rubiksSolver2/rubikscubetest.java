991f85f7b0aa1a22777144f0492b6a002772e455
NGreski <nj06gres@siena.edu>
2024-04-04 14:13:52 -0400

    small edits for number of moves to print

---
+++ rubiksSolver2/rubikscubetest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class rubikscubetest {
+    public static void main (String[] args){
+        rubikscube r=new rubikscube(2);
+        r.scramble();
+        r.solve();
+
+        //commented out by Noah, not looking at 3x3 cubes
+        // rubikscube c=new rubikscube(3);
+        // c.scramble();
+        // c.solve();
+    }
+}
