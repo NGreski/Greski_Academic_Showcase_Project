17528cdcfa9833b95397e6f20f3b9fad0f146fcf
NGreski <nj06gres@siena.edu>
2024-04-03 20:30:47 -0400

    added 2 rubiks algorithms code

---
+++ rubiksSolver2/Driver.java
@@ -0,0 +1,24 @@
+/**
+ * CODE FROM just-jay
+ * EDITED BY NOAH GRESK
+ */
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Cube c = new Cube();
+
+		double averageMoves = 0;
+		for(int i=0; i < 10; i++){ //edited to 10 from 10k
+			String s = Cube.generateScramble();		
+			c.scramble(s);
+			//System.out.println(c.Solve());
+			averageMoves+= c.Solve();	
+		} 
+		System.out.println("Average Moves: " + averageMoves/10); //edited to 10 from 10k
+	}
+}
+
+//39.66538
+//39.70893
